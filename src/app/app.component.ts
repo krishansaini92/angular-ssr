72d118f17f6f5c2bb911d7521f2cf64e36945b0a
Krishan <krishan.saini@trantorinc.com>
2021-01-23 15:40:16 +0530

    feat: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Krishan Saini';
+
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Angular, Universal, Example'},
+      {name: 'description', content: 'Krishan saini is a good boy'},
+      {name: 'robots', content: 'index, follow'}
+    ]);
+  }
 }
